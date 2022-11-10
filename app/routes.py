39c8ecd8a3cf013bc6b29f5f0b61897913462485
Sebastian Vargas <sebvargas@gmail.com>
2022-11-09 22:51:56 -0800

    chp3

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash
+from app.forms import LoginForm
 
 @app.route('/')
 def index():
@@ -15,3 +16,12 @@ def index():
     }
 ]
     return render_template('index.html', title = 'Home', user = user, posts = posts)
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember me = {form.remember_me.data}')
+        return redirect('/')
+    return render_template('login.html', title = 'Login', form = form)
