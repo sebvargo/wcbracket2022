cedb9bb83079ca0096e07f4be03217441f047e70
Sebastian Vargas <sebvargas@gmail.com>
2022-11-12 08:36:22 -0800

    heroku deployment changes

--- config.py
+++ config.py
@@ -2,9 +2,11 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
-    if SQLALCHEMY_DATABASE_URI is None:
-         SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-    else:
-        SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    try:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
+    except:
+        if SQLALCHEMY_DATABASE_URI is None:
+            SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+        else:
+            SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
