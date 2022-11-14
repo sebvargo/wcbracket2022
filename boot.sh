ad22275d7a92f568310ab2fc566ee6f49620e95f
Sebastian Vargas <sebvargas@gmail.com>
2022-11-13 23:54:47 -0800

    prepping gcp

--- boot.sh
+++ boot.sh
@@ -8,4 +8,4 @@ while true; do
     echo Upgrade command failed, retrying in 5 secs...
     sleep 5
 done
-exec gunicorn --bind :5000 --workers 3 --threads 8 --timeout 0 main:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - quiniela:app --workers=3 --threads=8 --worker-class=gthread
