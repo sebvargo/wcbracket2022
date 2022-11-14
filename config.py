d1860fc05aa9b2a279aece943e8617978655a881
Sebastian Vargas <sebvargas@gmail.com>
2022-11-14 02:04:01 -0800

    prepping gcp

--- config.py
+++ config.py
@@ -4,8 +4,14 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
     SESSION_PERMANENT =  False
     SESSION_TYPE = 'filesystem'
+    DATABASE_USER = os.environ["DB_USER"]  # e.g. 'my-database-user'
+    DATABASE_PASSWORD = os.environ["DB_PASS"]  # e.g. 'my-database-password'
+    DATABASE_NAME = os.environ["DB_NAME"]  # e.g. 'my-database'
+    INSTANCE_UNIX_SOCKET = os.environ["INSTANCE_UNIX_SOCKET"]  # e.g. '/cloudsql/project:region:instance'
+
     try:
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
+        SQLALCHEMY_DATABASE_URI = f'postgresql+pg8000://{DATABASE_USER}:{DATABASE_PASSWORD}@/{DATABASE_NAME}?unix_sock={INSTANCE_UNIX_SOCKET}/.s.PGSQL.5432'
+        # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
     except:
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
         if SQLALCHEMY_DATABASE_URI is None:
