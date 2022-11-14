ef09eab1a13a89329c5df04e37149006f7c2e134
Sebastian Vargas <sebvargas@gmail.com>
2022-11-13 17:27:27 -0800

    changes to try to fix heroku

--- app/routes.py
+++ app/routes.py
@@ -28,11 +28,11 @@ def login():
             flash('Invalid Username or Password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-        
+        return redirect(url_for('index'))    
+        # next_page = request.args.get('next')
+        # if not next_page or url_parse(next_page).netloc != '':
+        #     next_page = url_for('index')
+        # return redirect(next_page) 
     return render_template('login.html', title = 'Login', form = form)
 
 @app.route('/upload', methods = ['GET', 'POST'])
