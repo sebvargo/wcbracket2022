529d718432642a3b538fa81820db12e73d21603b
Sebastian Vargas <sebvargas@gmail.com>
2022-11-20 02:56:34 -0800

    adding Qatar 2022 quinielas before start of WC

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config(object):
     db_pass = os.environ.get('DB_PASS')
     db_name = os.environ.get('DB_NAME')
     db_instance = os.environ.get('DB_INSTANCE') 
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') \
-    #     or 'postgresql://postgres:memes@localhost:5432/quiniela-dev' #or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') \
+        or 'postgresql://postgres:memes@localhost:5432/quiniela-dev' #or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # if os.environ.get('DATABASE_TYPE') == 'postgres':
