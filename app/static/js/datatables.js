158cb680ef8df1713b9ce09f36100032d5127d93
Sebastian Vargas <sebvargas@gmail.com>
2022-11-30 21:48:04 -0800

    adjusted datatables layout

---
+++ app/static/js/datatables.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#example').DataTable();});
+$(document).ready(function () {
+    $('#rankings-table').DataTable();});
+    $('#rankings-table').dataTable( {
+          "lengthChange": false
+    } );
+$(document).ready(function () {
+    $('#predictions-table').DataTable();});
+    $('#predictions-table').dataTable( {
+          "lengthChange": false
+    } );
+$(document).ready(function () {
+    $('#calendar-table').DataTable();});
+    $('#calendar-table').dataTable( {
+          "lengthChange": false,
+        "ordering": false
+    } );
+$(document).ready(function () {
+    $('#allpredictions-table').DataTable();});
+    $('#allpredictions-table').dataTable( {
+          "lengthChange": false
+    } );
